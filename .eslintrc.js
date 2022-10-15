c99543b499628717222b6a37602acb5513f7837c
Kartik <43693807+KartikT900@users.noreply.github.com>
2022-10-15 23:11:50 +0530

    feat: added deps, lint setup and husky hooks

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+    node: true,
+    es2020: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:@typescript-eslint/strict',
+    'plugin:prettier/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json']
+  },
+  plugins: ['@typescript-eslint'],
+  settings: {
+    'import/resolver': {
+      node: {
+        extension: ['.js', '.ts', '.tsx', '.json']
+      }
+    }
+  },
+  root: true,
+  rules: {
+    'no-console': [
+      process.env.NODE_ENV === 'debug' ? 0 : 2,
+      { allow: ['warn', 'error'] }
+    ]
+  }
+};
---
+++ jest.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  preset: 'ts-jest',
+  modulePaths: ['<rootDir>/src'],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '<rootDir>/(config|public)/'
+  ],
+  testRegex: '((\\.|/)(test|spec))\\.ts?$',
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70
+    }
+  },
+  collectCoverage: true,
+  collectCoverageFrom: ['**/*.*', '!**/**/*.json'],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    './api',
+    './config',
+    './coverage',
+    '<rootDir>/src/pages',
+    '.config.js$',
+    '.eslintrc.js',
+    '.babel.config.js',
+    '<rootDir>/src/index.ts',
+    './public'
+  ],
+  coverageReporters: ['html'],
+  setupFilesAfterEnv: ['<rootDir>/src/test/setupTest.js'],
+  testEnvironment: 'jsdom'
+};
