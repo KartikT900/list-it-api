504fae31dc6b8f87ac00229e996add4b0372716d
Kartik <43693807+KartikT900@users.noreply.github.com>
2022-10-22 17:38:20 +0530

    feat: added unit test

---
+++ setupTest.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from '@prisma/client';
+import {
+  mockDeep,
+  mockReset,
+  DeepMockProxy
+} from 'jest-mock-extended';
+
+import prisma from './prisma/prismaClient';
+
+jest.mock('./prisma/prismaClient', () => ({
+  __esModule: true,
+  default: mockDeep<PrismaClient>()
+}));
+
+beforeEach(() => {
+  mockReset(prismaMock);
+});
+
+export const prismaMock =
+  prisma as unknown as DeepMockProxy<PrismaClient>;
