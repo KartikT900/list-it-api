7061cf6df6a3924c5180190d256da9406d847799
Kartik <43693807+KartikT900@users.noreply.github.com>
2022-11-05 19:28:23 +0530

    feat: add basic routing

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,1 @@
 import './server';
-import getAllUsersCount from './service/sample.service';
-
-async function main() {
-  try {
-    const users = await getAllUsersCount();
-    // eslint-disable-next-line no-console
-    console.info(`db users count:${users}`);
-  } catch (e) {
-    // error handling
-  }
-}
-
-void main();
