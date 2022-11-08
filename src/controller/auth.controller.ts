e4d09755d336ace0c0f4ac1b7ebf6b6d42ab9f56
Kartik <43693807+KartikT900@users.noreply.github.com>
2022-11-08 21:17:19 +0530

    feat: added auth controller
    Auth setup done using Auth0 as a provider

---
+++ src/controller/auth.controller.ts
@@ -0,0 +1,37 @@
+import { Response, Router } from 'express';
+import { Request as JWTRequest } from 'express-jwt';
+
+import { users } from 'models/user';
+
+import getUserById from '../service/user.service';
+
+const router = Router();
+
+/**
+ * @auth required
+ * @route {POST} /authorize
+ * @returns number: total count of users
+ */
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
+router.post('/authorize', async (req: JWTRequest, res: Response) => {
+  const user_id: string | undefined = req.auth?.sub?.split('|')[1];
+
+  if (!user_id) {
+    res.status(401).json({ message: 'invalid token' });
+    return;
+  }
+
+  try {
+    const userDetails: users | null = await getUserById(user_id);
+
+    if (userDetails) {
+      res.status(200).json(userDetails);
+    } else {
+      res.status(404).json({ message: 'user does not exist' });
+    }
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+export default router;
